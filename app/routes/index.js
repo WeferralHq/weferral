7500a54a16a32864c360544d1ab5955739f9a133
Ezehuche <ezehuche4@gmail.com>
2021-02-19 15:36:33 +0100

    removing unused components

--- app/routes/index.js
+++ app/routes/index.js
@@ -21,34 +21,8 @@ import Cards from './Cards/Cards';
 import CardsHeaders from './Cards/CardsHeaders';
 
 import NavbarOnly from './Layouts/NavbarOnly';
-import SidebarDefault from './Layouts/SidebarDefault';
-import SidebarA from './Layouts/SidebarA';
-import DragAndDropLayout from './Layouts/DragAndDropLayout';
-import SidebarWithNavbar from './Layouts/SidebarWithNavbar';
 import ParticipantNavBar from './Layouts/ParticipantNavBar';
 
-import Accordions from './Interface/Accordions';
-import Alerts from './Interface/Alerts';
-import Avatars from './Interface/Avatars';
-import BadgesLabels from './Interface/BadgesLabels';
-import Breadcrumbs from './Interface/Breadcrumbs';
-import Buttons from './Interface/Buttons';
-import Colors from './Interface/Colors';
-import Dropdowns from './Interface/Dropdowns';
-import Images from './Interface/Images';
-import ListGroups from './Interface/ListGroups';
-import MediaObjects from './Interface/MediaObjects';
-import Modals from './Interface/Modals';
-import Navbars from './Interface/Navbars';
-import Paginations from './Interface/Paginations';
-import ProgressBars from './Interface/ProgressBars';
-import TabsPills from './Interface/TabsPills';
-import TooltipPopovers from './Interface/TooltipsPopovers';
-import Typography from './Interface/Typography';
-import Notifications from './Interface/Notifications';
-import CropImage from './Interface/CropImage';
-import DragAndDropElements from './Interface/DragAndDropElements';
-import Calendar from './Interface/Calendar';
 import ReCharts from './Graphs/ReCharts';
 
 import DatePicker from './Forms/DatePicker';
@@ -76,7 +50,6 @@ import ReferralNewPassword from "./Pages/ReferralNewPassword";
 
 import Tables from './Tables/Tables';
 import ExtendedTable from './Tables/ExtendedTable';
-import AgGrid from './Tables/AgGrid';
 
 import AccountEdit from './Apps/AccountEdit';
 import BillingEdit from './Apps/BillingEdit';
@@ -182,34 +155,7 @@ export const RoutedContent = () => {
 
                 { /*    Layouts     */}
                 <Route path='/layouts/navbar' component={NavbarOnly} />
-                <Route path='/layouts/sidebar' component={SidebarDefault} />
-                <Route path='/layouts/sidebar-a' component={SidebarA} />
-                <Route path="/layouts/sidebar-with-navbar" component={SidebarWithNavbar} />
-                <Route path='/layouts/dnd-layout' component={DragAndDropLayout} />
 
-                { /*    Interface Routes   */}
-                <Route component={Accordions} path="/interface/accordions" />
-                <Route component={Alerts} path="/interface/alerts" />
-                <Route component={Avatars} path="/interface/avatars" />
-                <Route component={BadgesLabels} path="/interface/badges-and-labels" />
-                <Route component={Breadcrumbs} path="/interface/breadcrumbs" />
-                <Route component={Buttons} path="/interface/buttons" />
-                <Route component={Colors} path="/interface/colors" />
-                <Route component={Dropdowns} path="/interface/dropdowns" />
-                <Route component={Images} path="/interface/images" />
-                <Route component={ListGroups} path="/interface/list-groups" />
-                <Route component={MediaObjects} path="/interface/media-objects" />
-                <Route component={Modals} path="/interface/modals" />
-                <Route component={Navbars} path="/interface/navbars" />
-                <Route component={Paginations} path="/interface/paginations" />
-                <Route component={ProgressBars} path="/interface/progress-bars" />
-                <Route component={TabsPills} path="/interface/tabs-pills" />
-                <Route component={TooltipPopovers} path="/interface/tooltips-and-popovers" />
-                <Route component={Typography} path="/interface/typography" />
-                <Route component={Notifications} path="/interface/notifications" />
-                <Route component={CropImage} path="/interface/crop-image" />
-                <Route component={DragAndDropElements} path="/interface/drag-and-drop-elements" />
-                <Route component={Calendar} path="/interface/calendar" />
 
                 { /*    Forms Routes    */}
                 <Route component={DatePicker} path="/forms/date-picker" />
@@ -241,7 +187,6 @@ export const RoutedContent = () => {
                 { /*    Tables Routes   */}
                 <Route component={Tables} path="/tables/tables" />
                 <Route component={ExtendedTable} path="/tables/extended-table" />
-                <Route component={AgGrid} path="/tables/ag-grid" />
 
                 { /*    Apps Routes     */}
                 <Route component={AccountEdit} path="/apps/account-edit" />
@@ -308,10 +253,6 @@ export const RoutedNavbars  = () => (
             component={ NavbarOnly.Navbar }
             path="/layouts/navbar"
         />
-        <Route
-            component={ SidebarWithNavbar.Navbar }
-            path="/layouts/sidebar-with-navbar"
-        />
         { /* Default Navbar: */}
         <Route
             component={ DefaultNavbar }
@@ -325,10 +266,6 @@ export const RoutedSidebars = () => (
         <Route
             component={ SidebarASidebar }
             path="/layouts/sidebar-a"
-        />
-        <Route
-            component={ SidebarWithNavbar.Sidebar }
-            path="/layouts/sidebar-with-navbar"
         />
         { /* Default Sidebar: */}
         <Route
